fc8ac35a12da6f5a29c0b7ab36d784f139fb768d
Alexandre Demeure <alxdmr2@yahoo.fr>
2015-10-22 16:20:35 +0200

    reinit IHM

--- IHM/js/data.js
+++
@@ -1,9 +0,0 @@
-var data;
-
-if(localStorage && localStorage.dataIHM_TActHab) {
-	data = localStorage.dataIHM_TActHab;
-} else {data = {};}
-
-//______________________________________________________________________________________
-// Export Activity constructor
-module.exports = data;
