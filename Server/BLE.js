a1076250212d9ab0d6b6ea62686dfe48553b2000
Alexandre Demeure <alxdmr2@yahoo.fr>
2015-12-11 20:00:55 +0100

    req -> res

--- Server/BLE.js
+++ Server/BLE.js
@@ -5,11 +5,11 @@ var BrickBLE_server	= require( "../TactHab_modules/Bricks/BLE/BrickBLE_server.js
 module.exports = function(webServer) {
 	webServer.app.get	( "/BLE_init"
 						, function(req, res) {
-							req.end( BrickBLE_server.init() );
+							res.end( BrickBLE_server.init() );
 						});
 	webServer.app.get	( "/BLE_isInit"
 						, function(req, res) {
-							req.end( BrickBLE_server.isInit() );
+							res.end( BrickBLE_server.isInit() );
 						});
 }
 
