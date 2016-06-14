2b1a9ba41de7e0293c966d5063ecaad193627de9
AlexD <alxdmr2@yahoo.fr>
2016-06-14 19:38:45 +0200

    BLE

--- Server/BLE.js
+++ Server/BLE.js
@@ -1,7 +1,12 @@
+var BrickBLE_server = function() {
+	console.error( "The system was not able to load BLE module properly..." );
+};
+
 try {
-	require( "../TactHab_modules/Bricks/BLE/BrickBLE_server.js" );
+	BrickBLE_server = require( "../TactHab_modules/Bricks/BLE/BrickBLE_server.js" );
 	console.log( "BrickBLE_server successfully loaded" );
 } catch(errorBLE) {
 	console.error( "BrickBLE_server error:", errorBLE );
 }
-  
+
+module.exports = BrickBLE_server;
