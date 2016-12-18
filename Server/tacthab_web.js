96ecc01bb4d4ff0c71b252906681a7a8f54a240e
AlexD <alxdmr2@yahoo.fr>
2016-12-18 13:34:45 +0100

    more logs for socketBus...

--- Server/tacthab_web.js
+++ Server/tacthab_web.js
@@ -57,12 +57,14 @@ socketBus.on( "pong", function(msg) {
 
 
 socketBus.disconnectFrom	= function( host, login ) {
+    console.log("disconnectFrom", host, "login =", login);
 	var id = host + "::" + login;
 	if(this.connections[id]) {
 		this.connections[id].socket.off();
 		delete this.connections[id];
+		console.log( "Now connections are", Object.keys(this.connections) );
 	}
-}
+};
 
 socketBus.getConnections	= function() {
 	var json = {}, i;
@@ -74,7 +76,7 @@ socketBus.getConnections	= function() {
 		}
 	}
 	return json;
-}
+};
 
 socketBus.send				= function(host, login, pass, title, message) {
 	request.post( host + '/broadcast' ).form( 
@@ -83,7 +85,7 @@ socketBus.send				= function(host, login, pass, title, message) {
 			title 		: title,
 			message 	: message
 		});	
-}
+};
 
 socketBus.connectTo	= function( host, login, pass, friendlyName ) {
 	var self = this;
@@ -101,47 +103,52 @@ socketBus.connectTo	= function( host, login, pass, friendlyName ) {
 									 console.log("login =>", res);
 									 self.emit( "message", {socket: "login", res:res, ms: Date()});
 									 if(res === 'banco') {
-									 	self.setfriendlyName( friendlyName );
-										socket.on 	( 'disconnect'
-													, function() { 
-														console.log( "socketBus disconnect" );
-														self.emit( "message", {socket: "disconnect", ms: Date()});
-														self.disconnectFrom(host, login);
-														setTimeout( function() {
-															self.connectTo( host, login, pass, friendlyName );
-														}, 1000 );
-													});
-										socket.emit	( "subscribe"
-													, { id		: 'all'
-													  , data	: { title	: '.*'
-																  , regexp	: true }
-													  }
-													);
-										socket.on	( 'all'
-													, function(data) {
-														 console.log("socketBus receives", data);
-														 self.emit	( data.title
-														 			, { data	: data.message,
-																	 	login 	: login,
-																	 	pass	: pass,
-																	 	host	: host
-														 			} );
-														 self.emit	( "message"
-														 			, { data	: data,
-																	 	login 	: login,
-																	 	pass	: pass,
-																	 	host	: host
-														 			} );
-														}
-													);
-										self.ping();
-										socketBus.emit( 'connected', {
-											host 			: host, 
-											login 			: login, 
-											friendlyName	: friendlyName,
-											external_IP_v4	: external_IP_v4
-										} );
-										}
+											self.setfriendlyName( friendlyName );
+											console.log("\tsubscribe to disconnect");
+											socket.on 	( 'disconnect'
+														, function() {
+															console.log( "socketBus disconnect" );
+															self.emit( "message", {socket: "disconnect", ms: Date.now()});
+															self.disconnectFrom(host, login);
+															setTimeout( function() {
+																self.connectTo( host, login, pass, friendlyName );
+															}, 1000 );
+														});
+                                         	console.log("\temit subscribe");
+											socket.emit	( "subscribe"
+														, { id		: 'all'
+														  , data	: { title	: '.*'
+																	  , regexp	: true }
+														  }
+														);
+                                         	console.log("\tsubscribe to all");
+											socket.on	( 'all'
+														, function(data) {
+															 console.log("socketBus receives", data);
+															 self.emit	( data.title
+																		, { data	: data.message,
+																			login 	: login,
+																			pass	: pass,
+																			host	: host
+																		} );
+															 self.emit	( "message"
+																		, { data	: data,
+																			login 	: login,
+																			pass	: pass,
+																			host	: host
+																		} );
+															}
+														);
+                                         	console.log("\tping");
+											self.ping();
+                                         	console.log("\temit connected");
+											socketBus.emit( 'connected', {
+												host 			: host,
+												login 			: login,
+												friendlyName	: friendlyName,
+												external_IP_v4	: external_IP_v4
+											} );
+									 	}
 									} // socket.emit( 'login' ... )
 								);
 					}
